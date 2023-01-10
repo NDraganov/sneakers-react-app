2fbaa4e00309fd0add50b1acb281fb118914eb8a
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-01-10 22:47:28 +0000

    Removed NavLink component and replaced with regular list menu links.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,30 +1,26 @@
 import React from "react";
 import "./navigation.css";
-import NavLink from "./NavLink";
 
 function Navigation() {
   return (
-    <div className="container navigation">
-      <img className="toggle" src="assets/images/icon-menu.svg" alt="toggle" />
-      <div>
+    <div className="navigation">
+      <header>
+        <img className="toggle" src="assets/images/icon-menu.svg" alt="toggle"/>
         <img className="logo" src="assets/images/logo.svg" alt="logo" />
-      </div>
-      <nav>
         <ul className="menu-links">
-          <NavLink className="link" text="Collection" />
-          <NavLink text="Men" />
-          <NavLink text="Women" />
-          <NavLink text="About" />
-          <NavLink text="Contact" />
+          <li><a className="link">Collections</a></li>
+          <li><a className="link">Men</a></li>
+          <li><a className="link">Women</a> </li>
+          <li><a className="link">About</a></li>
+          <li><a className="link">Contact</a></li>
         </ul>
-        <div>
-          <img className="cart" src="assets/images/icon-cart.svg" alt="cart" />
+        <div className="right-nav">
+          <div className="cart-container">
+            <img className="cart" src="assets/images/icon-cart.svg" alt="cart"/>
+          </div>
+            <img className="avatar" src="assets/images/image-avatar.png" alt="avatar"/>
         </div>
-        <div>
-          <img className="avatar" src="assets/images/image-avatar.png" alt="avatar" />
-        </div>
-        
-      </nav>
+      </header>
     </div>
   );
 }
