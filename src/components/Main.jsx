5f8b11c8e10f6908906528aa3783f7b61d7d904d
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-01-11 00:25:33 +0000

    Created Gallery component and linked to Main. Added source for images.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,18 +2,33 @@ import React from "react";
 import bootstrap from 'bootstrap';
 import "./main.css";
 import Description from "./Description";
+import Gallery from "./Gallery";
 
 function Main() {
+    const galleryImagesSmall = [
+        {img: 'assets/images/image-product-1-thumbnail.jpg'},
+        {img: "assets/images/image-product-2-thumbnail.jpg"},
+        {img: "assets/images/image-product-3-thumbnail.jpg"},
+        {img: "assets/images/image-product-4-thumbnail.jpg"}
+    ]
+    const galleryImagesBig = [
+        {img: 'assets/images/image-product-1.jpg'},
+        {img: "assets/images/image-product-2.jpg"},
+        {img: "assets/images/image-product-3.jpg"},
+        {img: "assets/images/image-product-4.jpg"}
+    ]
     return (
         <div className="row">
             <div className="col-lg-6">
-                
+                <Gallery
+                galleryImagesSmall={galleryImagesSmall}
+                galleryImagesBig={galleryImagesBig} />
             </div>
             <div className="col-lg-6">
                 <Description />
             </div>
         </div>
-    )
+    );
 }
 
 export default Main;
