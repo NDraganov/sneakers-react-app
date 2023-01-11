760348a4d5acc9297b3c5362f0e14162cfdfcdb5
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-01-11 00:08:49 +0000

    Added Description component to Main.

---
+++ src/components/Description.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./description.css";
+
+function Description() {
+  return (
+    <div className="description">
+        <p className="sneaker-company">SNEAKER COMPANY</p>
+        <h2 className="description-title">Fall Limited Edition Sneakers</h2>
+        <p className="sneakers-description">These low-profile sneakers are your perfect casual wear companion. 
+        Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
+    </div>
+  );
+}
+export default Description;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import bootstrap from 'bootstrap';
 import "./main.css";
+import Description from "./Description";
 
 function Main() {
     return (
-        <div>
-            
+        <div className="row">
+            <div className="col-lg-6">
+                
+            </div>
+            <div className="col-lg-6">
+                <Description />
+            </div>
         </div>
     )
 }
