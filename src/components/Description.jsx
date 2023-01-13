5fae24a5509d18462ab15fc17210718b7b7cc4bf
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-01-13 00:17:11 +0000

    Added sale price section in Description.

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import './description.css';
+import React from "react";
+import "./description.css";
 
 function Description() {
   return (
     <div className="description">
-        <p className="sneaker-company">SNEAKER COMPANY</p>
-        <h2 className="description-title">Fall Limited Edition Sneakers</h2>
-        <p className="sneakers-description">These low-profile sneakers are your perfect casual wear companion. 
-        Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
+      <p className="sneaker-company">SNEAKER COMPANY</p>
+      <h2 className="description-title">Fall Limited Edition Sneakers</h2>
+      <p className="sneakers-description">
+        These low-profile sneakers are your perfect casual wear companion.
+        Featuring a durable rubber outer sole, they’ll withstand everything the
+        weather can offer.
+      </p>
+      <div className="price">
+        <div className="sale-section">
+          <p className="sale-price">$125.00</p>
+          <div className="transbox">
+            <p className="sale">50%</p>
+          </div>
+        </div>
+        <p className="regular-price">$250.00</p>
+      </div>
     </div>
   );
 }
