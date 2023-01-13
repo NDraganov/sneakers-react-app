22d4ad53a052927115e9e776d5612a2c9afb82c4
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-01-13 12:14:38 +0000

    Added State for count number(increase and decrease). Added on hover properties for buttons.

--- src/components/AddCart.jsx
+++ src/components/AddCart.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import "./add-cart.css";
 import RemoveOutlinedIcon from '@mui/icons-material/RemoveOutlined';
 import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 
 function AddCart() {
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+  function decrease() {
+    setCount(count - 1);
+  }
+
   return (
     <div className="container add">
       <div className="row">
         <div className="col-lg-3 col-md-12 count-buttons">
-          <button className="btn"><RemoveOutlinedIcon /></button>
-            <span className="number"></span>
-          <button className="btn"><AddOutlinedIcon /></button>
+          <button className="btn" onClick={decrease}><RemoveOutlinedIcon /></button>
+            <span className="number">{count}</span>
+          <button className="btn" onClick={increase}><AddOutlinedIcon /></button>
         </div>
         <div className="col-lg-6 col-md-12 add-to-cart">
           <button className="add-btn">
