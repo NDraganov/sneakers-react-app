f0034bc2162f61f9a1a723746e829fe0d7770e1c
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-01-16 00:31:19 +0000

    Added Checkout module on click event for cart and avatar.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,6 +4,7 @@ import "./navigation.css";
 
 function Navigation() {
   const [data, setData] = useState(null);
+  const [cart, setCart] = useState(false);
   const [sideNav, setSideNav] = useState(false);
 
   {/* useEffect info taken from - https://www.youtube.com/watch?v=Dorf8i6lCuk */}
@@ -11,6 +12,15 @@ function Navigation() {
     const get = JSON.parse(localStorage.getItem("data")) || null;
     setData(get);
   }, [data])
+
+  function handleCart() {
+    setCart(!cart);
+  }
+
+  function removeData() {
+    localStorage.removeItem('data');
+    window.location.reload();
+  }
 
   function handleOpenOffCanvas() {
     setSideNav(true)
@@ -30,10 +40,10 @@ function Navigation() {
         </ul>
         <div className="right-nav">
           <div className="cart-container">
-            <img className="cart" src="assets/images/icon-cart.svg" alt="cart"/>
+            <img className="cart" src="assets/images/icon-cart.svg" alt="cart" onClick={handleCart}/>
             <p className={data > 0 && `count-cart`}>{data}</p>
           </div>
-            <img className="avatar" src="assets/images/image-avatar.png" alt="avatar"/>
+            <img className="avatar" src="assets/images/image-avatar.png" alt="avatar" onClick={handleCart}/>
         </div>
       </header>
       <hr />
@@ -46,6 +56,30 @@ function Navigation() {
         <p>Women</p>
         <p>About</p>
         <p>Contact</p>
+      </div>
+
+      <div className={`container modul ${cart === false && `hidden`}`}>
+        <h5>Cart</h5>
+        <hr />
+        {data === null ? (
+          <div className="empty-cart">
+            <p className="empty-info">Your cart is empty</p>
+          </div>
+          ) : (
+          <div className="cart-info">
+            <div className="content">
+              <img className="thumb" src="assets/images/image-product-1-thumbnail.jpg" alt=""/>
+              <div>
+                <p>Fall Limited Edition Sneakers</p>
+                <p>$125.00 x {data} &nbsp;
+                  <span>${125 * data}.00</span>
+                </p>
+              </div>
+              <button className="bin" onClick={removeData}><img src="assets/images/icon-delete.svg" alt="" /></button>
+            </div>
+            <button className="checkout-btn">Checkout</button>
+          </div>
+        )}
       </div>
     </div>    
   );
