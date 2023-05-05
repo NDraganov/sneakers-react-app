b1a871480934a0167aa5feb8530194698a28d08d
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-05-05 11:48:30 +0100

    Change styles for header

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,72 +1,102 @@
-import React, {useState, useEffect} from 'react';
-import bootstrap from 'bootstrap';
-import './navigation.css';
-import toggle from '../images/icon-menu.svg';
-import logo from '../images/logo.svg';
-import basket from '../images/icon-cart.svg';
-import avatar from '../images/image-avatar.png';
-import bin from '../images/icon-delete.svg';
-import CloseIcon from '@mui/icons-material/Close';
+import React, { useState, useEffect } from "react";
+import "./navigation.css";
+import toggle from "../images/icon-menu.svg";
+import logo from "../images/logo.svg";
+import basket from "../images/icon-cart.svg";
+import avatar from "../images/image-avatar.png";
+import bin from "../images/icon-delete.svg";
+import CloseIcon from "@mui/icons-material/Close";
 
 function Navigation() {
   const [data, setData] = useState(null);
   const [cart, setCart] = useState(false);
   const [sideNav, setSideNav] = useState(false);
-  const offcanvasBackground = document.getElementById('offcanvas-background');
+  const offcanvasBackground = document.getElementById("offcanvas-background");
 
-  {/* useEffect info taken from - https://www.youtube.com/watch?v=Dorf8i6lCuk */}
+  {
+    /* useEffect info taken from - https://www.youtube.com/watch?v=Dorf8i6lCuk */
+  }
   useEffect(() => {
     const get = JSON.parse(localStorage.getItem("data")) || null;
     setData(get);
-  }, [data])
+  }, [data]);
 
   function handleCart() {
     setCart(!cart);
   }
 
   function removeData() {
-    localStorage.removeItem('data');
+    localStorage.removeItem("data");
     window.location.reload();
   }
 
   function handleOpenOffcanvas() {
-    setSideNav(true)
-    offcanvasBackground.classList.add('offcanvas-background');
+    setSideNav(true);
+    offcanvasBackground.classList.add("offcanvas-background");
   }
   function handleCloseOffcanvas() {
     setSideNav(false);
-    offcanvasBackground.classList.remove('offcanvas-background');
+    offcanvasBackground.classList.remove("offcanvas-background");
   }
 
   return (
     <div className="navigation">
       {/* Main navigation */}
       <header>
-        <nav>
-        <img className="toggle" src={toggle} alt="toggle" onClick={handleOpenOffcanvas}/>
-        <img className="logo" src={logo} alt="logo" />
-        <ul className="menu-links">
-          <li><a className="link">Collections</a></li>
-          <li><a className="link">Men</a></li>
-          <li><a className="link">Women</a> </li>
-          <li><a className="link">About</a></li>
-          <li><a className="link">Contact</a></li>
-        </ul>
+        <div>
+          <img
+            className="toggle"
+            src={toggle}
+            alt="toggle"
+            onClick={handleOpenOffcanvas}
+          />
+          <img className="logo" src={logo} alt="logo" />
+          <nav>
+            <ul>
+              <li>
+                <p>Collections</p>
+              </li>
+              <li>
+                <p>Men</p>
+              </li>
+              <li>
+                <p>Women</p>{" "}
+              </li>
+              <li>
+                <p>About</p>
+              </li>
+              <li>
+                <p>Contact</p>
+              </li>
+            </ul>
+          </nav>
+        </div>
         <div className="right-nav">
           <div className="cart-container">
-            <img className="cart" src={basket} title='Cart' alt="cart" onClick={handleCart}/>
-            <p className={data > 0 && `count-cart`}>{data}</p>
+            <img
+              className="cart"
+              src={basket}
+              title="Cart"
+              alt="cart"
+              onClick={handleCart}
+            />
+            <p className={data > 0 ? `count-cart` : undefined}>{data}</p>
           </div>
-            <img className="avatar" src={avatar} title='Avatar' alt="avatar" onClick={handleCart}/>
+          <img
+            className="avatar"
+            src={avatar}
+            title="Avatar"
+            alt="avatar"
+            onClick={handleCart}
+          />
         </div>
-        </nav>
       </header>
-      <hr className='nav-line' />
+      <hr />
 
       {/* offcanvas */}
       <div className={`side-nav ${sideNav === false && `hidden`}`}>
         <button className="close-btn" onClick={handleCloseOffcanvas}>
-          <CloseIcon fontSize="medium"/>
+          <CloseIcon fontSize="medium" />
         </button>
         <p>Collections</p>
         <p>Men</p>
@@ -74,7 +104,7 @@ function Navigation() {
         <p>About</p>
         <p>Contact</p>
       </div>
-      <div id='offcanvas-background'></div>
+      <div id="offcanvas-background"></div>
       {/* Checkout modal */}
       <div className={`container modul ${cart === false && `hidden`}`}>
         <h5>Cart</h5>
@@ -83,23 +113,30 @@ function Navigation() {
           <div className="empty-cart">
             <p className="empty-info">Your cart is empty</p>
           </div>
-          ) : (
+        ) : (
           <div className="cart-info">
             <div className="content">
-              <img className="thumbnail" src={require("../images/image-product-1-thumbnail.jpg")} alt=""/>
+              <img
+                className="thumbnail"
+                src={require("../images/image-product-1-thumbnail.jpg")}
+                alt=""
+              />
               <div>
                 <p>Fall Limited Edition Sneakers</p>
-                <p>$125.00 x {data} &nbsp;
+                <p>
+                  $125.00 x {data} &nbsp;
                   <span>${125 * data}.00</span>
                 </p>
               </div>
-              <button className="bin" onClick={removeData}><img src={bin} alt="bin" /></button>
+              <button className="bin" onClick={removeData}>
+                <img src={bin} alt="bin" />
+              </button>
             </div>
             <button className="checkout-btn">Checkout</button>
           </div>
         )}
       </div>
-    </div>    
+    </div>
   );
 }
 
