a14041e92a68944678cab35b341f944143882f0e
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-05-06 17:50:13 +0100

    Change styles for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import './footer.css';
+import React from "react";
+import "./footer.css";
 
 function Footer() {
   return (
     <div className="footer">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" title='Frontend Mentor' target="_blank">Frontend Mentor</a>  
-        Coded by <a href="https://github.com/NDraganov" title='Nedyalko Draganov' target='_blank'>Nedyalko Draganov</a>
+      <div>
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          title="Frontend Mentor"
+          target="_blank"
+        >
+          Frontend Mentor
+        </a>
+      </div>
+      <div>
+        Coded by{" "}
+        <a
+          href="https://github.com/NDraganov"
+          title="Nedyalko Draganov"
+          target="_blank"
+        >
+          Nedyalko Draganov
+        </a>
+      </div>
     </div>
   );
 }
